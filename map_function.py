d05da4f06d1bf9a80ae756f95df88b6a5bb062e6
Jay <48065168+Yozora-7@users.noreply.github.com>
2022-10-30 11:20:34 +0000

    Create map_function.py

---
+++ map_function.py
@@ -0,0 +1,9 @@
+
+def test(i):
+  return i * i
+
+x - map(test(4, 6, 3, 2))
+
+"""
+If we were to print this, then the result would be all of the numbers squared. Map applies the function to every other element declared after it in the same iteration.
+"""
